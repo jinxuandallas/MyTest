7cdb73b64a70b2a866ebc98a0c650d3e0879b4bd
Dallas <dallas2k@163.com>
2021-07-24 17:14:47 +0800

    地图加上边框

--- Map2.gd
+++ Map2.gd
@@ -1,20 +1,16 @@
-extends TextureRect
+extends Node2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var map=self
+onready var map=$TextureRect
 onready var border=$ReferenceRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	test()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 func test():
 	
 #	print(map is Sprite)
@@ -29,31 +25,16 @@ func test():
 #	print(map.region_rect.size.x)
 #	print(map.region_rect.size.y)
 	print(map.get_rect().size.x)
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
 
 
-#func _on_53_ready():
-#	var map=$"."
-#	print(map.region_rect.size.x)
-#	print(map.region_rect.size.y)
 
-
-func _on_ReferenceRect_mouse_entered():
+func _on_TextureRect_mouse_entered():
 	border.visible=true
-	print("xx")
+	
 
 
-func _on_ReferenceRect_mouse_exited():
+func _on_TextureRect_mouse_exited():
 	border.visible=false
-	print("yy")
-
-
-func _on_53_mouse_entered():
-#	if border.visible==false:
-#		border.visible=true
-	print("xx")
-
-
-func _on_53_mouse_exited():
-#	if border.visible==true:
-#		border.visible=false
-	print("yy")
