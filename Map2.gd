7fac2e236fa478b045c38d24b426a241764316e2
Dallas <dallas2k@163.com>
2021-08-11 21:59:01 +0800

    初步研究完边缘检测和滚动等问题

--- Map2.gd
+++ Map2.gd
@@ -9,6 +9,7 @@ onready var border=$ReferenceRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+#	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
 	test()
 
 func test():
